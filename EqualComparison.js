5e3f68c80bdc5ea346a337ce3cbe19a23e08bb93
Amalnath384 <86220114+Amalnath384@users.noreply.github.com>
2021-06-22 07:55:11 +0530

    Add files via upload

---
+++ EqualComparison.js
@@ -0,0 +1,16 @@
+
+let a = "787";
+let b = "78";
+
+if (a == b) {
+    console.log("Values are same ");//== compares only the values
+}
+else {
+    console.log("fvalues are different");
+}
+if (a === b) {
+    console.log("data types are same");
+}
+else {
+    console.log("data type is different");//compares values and datatypes
+}
