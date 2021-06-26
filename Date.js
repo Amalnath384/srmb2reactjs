40810c7bd3983e5deb03df06b47996753a6d40e0
Amalnath384 <86220114+Amalnath384@users.noreply.github.com>
2021-06-26 07:32:39 +0530

    Add files via upload

--- Date.js
+++ Date.js
@@ -1,9 +1,19 @@
 console.log("PAST DATE");
-const today = new Date()
-const yesterday = new Date(today)
+const today1 = new Date()
+const yesterday = new Date(today1)
 
 yesterday.setDate(yesterday.getDate() - 1)
 
-today.toDateString()
+today1.toDateString()
 yesterday.toDateString()
 console.log("YESTERDAY DAY : " + yesterday);
+
+//date model
+var date2 = new Date(); 
+date2.setFullYear(2021, 5, 25);
+console.log(date2);
+
+//current date
+var today = new Date();
+var date = today.getFullYear()+'-'+today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getHours() + ":" +today.getMinutes() + ":" + today.getSeconds();
+console.log(date);
